5bfd8d436d0fe2a2da6b23613f26e60e006168c9
Mackenzie <mlbaciuska@gmail.com>
2021-12-12 00:11:20 -0500

    modifying tally score function

--- main.js
+++ main.js
@@ -19,40 +19,69 @@ return moves[Math.floor (Math.random() * moves.length)];
 */
 
 function playRound (playerSelection, computerSelection){
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
 
-    if (playerSelection=='Rock'&&computerSelection=='Scissors') {return 'Winner! Rock beats Scissors'}
-    else if (playerSelection=='Paper'&&computerSelection=='Rock') {return 'Winner! Paper beats Rock'}
-    else if (playerSelection=='Scissors'&&computerSelection=='Paper') {return 'Winner! Scissors beats Paper'}
-    else if (playerSelection == computerSelection) {return "It's a tie! You both selected " + playerSelection}
-    else  {return 'Oh no, you lost. ' + computerSelection + ' beats ' + playerSelection};
+    if (playerSelection=='rock'&&computerSelection=='scissors') {return 'Winner! Rock beats Scissors!'.toUpperCase()} 
+    else if (playerSelection=='paper'&&computerSelection=='rock') {return 'Winner! Paper beats Rock'.toUpperCase()}
+    else if (playerSelection=='scissors'&&computerSelection=='paper') {return 'Winner! Scissors beats Paper'.toUpperCase()}
+    else if (playerSelection == computerSelection) {return "It's a tie! You both selected ".toUpperCase() + playerSelection.toUpperCase()}
+    else  {return ('Oh no, you lost. ' + computerSelection+ ' beats ' + playerSelection).toUpperCase()};
 
 }
 
 const playerSelection = 'Rock';
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+
 
 /* Write a NEW function called game(). Use the previous function 
 inside of this one to play a 5 round game that keeps score and reports 
 a winner or loser at the end. */
 
-function game(){
+function game (round1,round2,round3,round4,round5) 
+{
+ 
+    //playRound
+    
+    round1 = console.log(playRound(playerSelection,computerSelection));
+    round2 = console.log(playRound(playerSelection,computerSelection));
+    round3 = console.log(playRound(playerSelection,computerSelection));
+    round4 = console.log(playRound(playerSelection,computerSelection));
+    round5 = console.log(playRound(playerSelection,computerSelection));
 
-    console.log (playRound(playerSelection,computerSelection));
-    console.log (playRound(playerSelection,computerSelection));
-    console.log (playRound(playerSelection,computerSelection));
-    console.log (playRound(playerSelection,computerSelection));
-    console.log (playRound(playerSelection,computerSelection));
-
+    //if Player wins add 1 point to playerScore, if player loses, add 1 to computerScore
+    
 }
 
-game(playerSelection, computerSelection);
+function tallyScore () {
+    
+    if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection =='rock') ||
+        (playerSelection == 'scissors' && computerSelection=='paper')
+    ) {return 1}
+    else if (playerSelection == computerSelection) {return 0}
+    else {return -1}
+    
+}
+let playerScore = game(playRound(tallyScore()));
+console.log(playerScore);
+/*
+make the function play 5 times
+if the player wins increase playerScore by 1
+if the player loses, increase computerScore by 1*/
+
 
 // - Prompt the user to enter their selection 
 
+/* create a function that plays a round and outputs a point for winner. 
+    if the player wins, the player gets 1 point
+        if player wins, increase playerScore by 1 point (++)
+    if the computer wins, the computer gets 1 point
+        if player does not win, increase computerScore by 1 point (++)
+    whoever gets 5 points first, is declared the winner
+        if playerScore ==5 OR computerScore ==5 {return }
 
 
 
-
-
-
+*/
